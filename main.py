51c7f6fe921d670d58703da0e34c067a03a7dc56
William-AViana <willdev.viana@gmail.com>
2025-10-09 21:45:14 -0300

    create new file and test functions for manipulation strings
    Signed-off-by: William-AViana <willdev.viana@gmail.com>

---
+++ main.py
@@ -0,0 +1,14 @@
+string = "  My name is William and i am learning data enginner."
+
+new_string = string.split(' ')
+print(new_string)
+
+string_join = string.join(new_string)
+print(string_join)
+
+string_strip = string.strip()
+print(string_strip)
+
+string_replace = string.replace(' ', '%20')
+print(string_replace)
+
